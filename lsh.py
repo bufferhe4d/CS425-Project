ac4d65860db5ad88815a5a95337b03d8e26f45db
Abdullah Talayhan <abdullahttj@gmail.com>
2019-04-29 19:42:03 +0300

    Change from Movies to Users
    Started Recommendation Generation

--- lsh.py
+++ lsh.py
@@ -11,20 +11,21 @@ class LSH:
         self.buckets = None
         self.vectors = None
         self.num_vector = None
+        self.div_num = 11
 
     def train(self, num_vector):
         dim = self.data.shape[1]
         self.num_vector = num_vector
         random_vectors = []
-        for i in range(10):
-            random_vectors.append(  np.random.randn(int(dim/10), num_vector) )
+        for i in range(self.div_num):
+            random_vectors.append(  np.random.randn(int(dim/self.div_num), num_vector) )
         bin_to_decimal = 1 << np.arange(num_vector - 1, -1, -1)
 
         table =[]
         bin_index_bits = []
         bin_index_arr = []
-        datasplitted =  np.hsplit( data, 10 )
-        for i in range(10):
+        datasplitted =  np.hsplit( data, self.div_num )
+        for i in range(self.div_num):
             table.append({})
             bin_index_bits.append( ( datasplitted[i].dot( random_vectors[i]) >= 0) )
             # Encode bin index bits into integers
@@ -39,16 +40,25 @@ class LSH:
     def query(self, movieId):
         similars = []
         bin_to_decimal = 1 << np.arange(self.num_vector - 1, -1, -1)
-        datasplitted =  np.hsplit( self.data, 10 )
-        for i in range(10):
+        datasplitted =  np.hsplit( self.data, self.div_num )
+        for i in range(self.div_num):
             bit_arr = datasplitted[i][movieId].dot(self.vectors[i]) >= 0
             bin_index = bit_arr.dot(bin_to_decimal)
             k = self.buckets[i][bin_index]
-            similars.extend(k)
+            for item in k:
+                if item not in similars:
+                    similars.append(item)
 
         return similars
 
-
+    def make_recommendation(self, movieId, similars):
+        sim_total = 0
+        sum_total = 0
+        for i in range(len(similars)):
+            cur_sim = 1 - spatial.distance.cosine(data[movieId], data[similars[i]])
+            sum_total += cur_sim*data[similars[i]][movieId]
+            sim_total += cur_sim
+        
 
 # configure file path
 data_path = 'ml-latest-small'
@@ -67,14 +77,14 @@ df_ratings = pd.read_csv(
 
 print(df_ratings.head(5))
 
-df_rating_matrix = df_ratings.pivot(index='movieId', columns='userId', values='rating').fillna(0)
+df_rating_matrix = df_ratings.pivot(index='userId', columns='movieId', values='rating').fillna(0)
 df_rating_matrix = df_rating_matrix.sub(df_rating_matrix.mean(axis=1), axis=0)
 data = df_rating_matrix.values
 
 model = LSH(data)
 print(data)
-
-model.train(10)
+print(data.shape)
+model.train(100)
 
 count = 0
 """ for i in model.buckets:
@@ -88,4 +98,4 @@ count = 0
 
 print("Count:", count)
 
-print(model.query(1))
+print(len(model.query(1)))
