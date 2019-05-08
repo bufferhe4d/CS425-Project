99447facb5dba8bebbab3b4e0f6c62bd2e5c9225
cinarmert <cinar.mrt@gmail.com>
2019-05-08 09:36:24 +0300

    add staff info to item-feature matrix

--- content_based/FeatureMatrixGenerator.py
+++ content_based/FeatureMatrixGenerator.py
@@ -6,9 +6,10 @@ from pandas import DataFrame
 
 class FeatureMatrixGenerator:
 
-    def __init__(self, data, top_tags):
+    def __init__(self, data, top_tags, top_staff):
         self.raw_data = data
         self.top_tags = top_tags
+        self.top_staff = top_staff
         self.item_feature_matrix = np.array([])
         self.genres = ["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama",
                        "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War",
@@ -28,7 +29,7 @@ class FeatureMatrixGenerator:
         # add 1 col representing the leading actor
         # 18 cols representing each genre in dataset
         # 9742 is the number of movies
-        self.item_feature_matrix = np.zeros((9742, 21), dtype="int")
+        self.item_feature_matrix = np.zeros((9742, 1519), dtype="int")
 
     def populate_matrix(self):
         for i, movie in enumerate(self.raw_data.movies):
@@ -43,16 +44,22 @@ class FeatureMatrixGenerator:
             #     tag_idx = np.where(self.top_tags == tag)[0][0] + 1
             #     self.item_feature_matrix[i][tag_idx] = 1
 
-            # populate director and actor cols
-            director, actor = self.get_director_and_actor(m_id)
-            self.item_feature_matrix[i][1] = director
-            self.item_feature_matrix[i][2] = actor
+            # populate actor cols - top 1499 staff
+            m_actors = self.get_actors_by_movie(m_id)
+            for actor in m_actors:
+                actor_idx = [i[0] for i in self.top_staff].index(actor) + 1
+                self.item_feature_matrix[i][actor_idx] = 1
+
+            # populate director and actor cols - 1 dir, 1 actor approach
+            # director, actor = self.get_director_and_actor(m_id)
+            # self.item_feature_matrix[i][1] = director
+            # self.item_feature_matrix[i][2] = actor
 
             # populate genres
             m_genres = self.get_genres_by_movie_id(m_id)
             for j, genre in enumerate(self.genres):
                 if genre in m_genres:
-                    self.item_feature_matrix[3 + j] = 1
+                    self.item_feature_matrix[i][1500 + j] = 1
 
             print("movie " + str(i) + " done")
 
@@ -83,6 +90,22 @@ class FeatureMatrixGenerator:
 
         return '1' + director[2:], '1' + actor[2:]
 
+    def get_actors_by_movie(self, m_id):
+        res = []
+
+        df_links = DataFrame(self.raw_data.links)
+        imdb_id = 'tt' + np.array(df_links.loc[df_links[0] == m_id])[0][1]
+
+        df_staff = DataFrame(self.raw_data.staff)
+        actors_by_mov = np.array(df_staff.loc[df_staff[0] == imdb_id])
+
+        for actor in actors_by_mov:
+            tmp = actor[1].lower().replace(" ", "")
+            if tmp in [i[0] for i in self.top_staff]:
+                res.append(tmp)
+
+        return res
+
     def get_tags_by_movie(self, m_id):
         res = []
 
@@ -100,6 +123,6 @@ class FeatureMatrixGenerator:
 if __name__ == '__main__':
     data = DataParser()
     data.parse_data()
-    stats = Statistics(data.tags)
-    fgm = FeatureMatrixGenerator(data, stats.get_top_tags())
+    stats = Statistics(data.tags, data.staff)
+    fgm = FeatureMatrixGenerator(data, stats.get_top_tags(), stats.get_top_staff())
     fgm.populate_matrix()
